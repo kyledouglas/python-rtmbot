b36e08f0e3e14a1592ccd57895627a30662a1718
Kyle Douglas <kyle.d.douglas@espn.com>
2016-09-12 09:08:20 -0400

    no idea

--- printBotId.py
+++ printBotId.py
@@ -7,13 +7,13 @@ BOT_NAME = 'starterbot'
 slack_client = SlackClient('xoxb-78623460805-yLNLhfdigcd9Cu3gRy8EKx6u')
 
 
-if __name__ == "__main__":
-    api_call = slack_client.api_call("users.list")
-    if api_call.get('ok'):
-        # retrieve all users so we can find our bot
-        users = api_call.get('members')
-        for user in users:
-            if 'name' in user and user.get('name') == BOT_NAME:
-                print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
-    else:
-        print("could not find bot user with the name " + BOT_NAME)
+
+api_call = slack_client.api_call("users.list")
+if api_call.get('ok'):
+    # retrieve all users so we can find our bot
+    users = api_call.get('members')
+    for user in users:
+        if 'name' in user and user.get('name') == BOT_NAME:
+            print("Bot ID for '" + user['name'] + "' is " + user.get('id'))
+else:
+    print("could not find bot user with the name " + BOT_NAME)
